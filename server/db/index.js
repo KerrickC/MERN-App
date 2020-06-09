d59141754781ad01f974f6a0a031740753c232d1
Kerrick Cavanaugh <kerrickcavanaugh@gmail.com>
2020-06-09 14:43:47 -0400

    fourth

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,5 @@
 
 const mongoose = require('mongoose')
-const expressSession  = require('express-session')
-const mongoStore = require('mongo-store')(expressSession)
-
-
-
 
 mongoose
     .connect('mongodb://127.0.0.1:27017/testdb', {useNewUrlParser: true}).then(()=>{
